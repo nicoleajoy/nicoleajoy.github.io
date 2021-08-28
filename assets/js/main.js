d8ec87ead2f274bfdb361555b90999aff65c5a24
Nicole Ajoy <nnx98a@gmail.com>
2021-08-28 16:42:28 -0400

    Update website

--- assets/js/main.js
+++ assets/js/main.js
@@ -164,12 +164,12 @@
   const typed = select('.typed')
   if (typed) {
     let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
+    typed_strings = typed_strings.split('|')
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
+      typeSpeed: 75,
+      backSpeed: 25,
       backDelay: 2000
     });
   }
